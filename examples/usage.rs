4383c60ca4b904b4a0c4133f78c040e54bc52e69
Jasm Sison <jasm.sison@gmail.com>
2023-02-23 19:13:16 +0100

    fmt

--- examples/usage.rs
+++ examples/usage.rs
@@ -1,28 +1,31 @@
 use std::rc::Rc;
 
 use flatbuffers::FlatBufferBuilder;
-use lean_buffer::{macros::LeanBufferWrite, traits::{Factory, FactoryExt, AdapterExt}};
+use lean_buffer::{
+    macros::LeanBufferWrite,
+    traits::{AdapterExt, Factory, FactoryExt},
+};
 
 // Don't panic when you see this false positive warning:
 // proc macro `LeanBufferWrite` not expanded: proc macro not found in the built dylib
 // Just check if the generated file can be located.
 #[derive(LeanBufferWrite)]
 struct Entity {
-  t_u64: u64,
-  t_i64: i64,
-  t_u32: u32,
-  t_i32: i32,
-  t_char: char,
-  t_u16: u16,
-  t_i16: i16,
-  t_u8: u8,
-  t_i8: i8,
-  t_bool: bool,
-  t_string: String,
-  t_vec_string: Vec<String>,
-  t_vec_u8: Vec<u8>,
-  t_double: f64,
-  t_float: f32,
+    t_u64: u64,
+    t_i64: i64,
+    t_u32: u32,
+    t_i32: i32,
+    t_char: char,
+    t_u16: u16,
+    t_i16: i16,
+    t_u8: u8,
+    t_i8: i8,
+    t_bool: bool,
+    t_string: String,
+    t_vec_string: Vec<String>,
+    t_vec_u8: Vec<u8>,
+    t_double: f64,
+    t_float: f32,
 }
 
 // Either copy this file from your project, or use the name convention
@@ -30,35 +33,35 @@ struct Entity {
 include!(concat!(env!("OUT_DIR"), "/Entity_lb_gen.rs"));
 
 fn main() {
-  let mut builder = FlatBufferBuilder::new();
+    let mut builder = FlatBufferBuilder::new();
 
-  let factory = Factory::<Entity>{
-    phantom_data: std::marker::PhantomData,
-  };
-  let f = Rc::new(factory) as Rc<dyn FactoryExt<Entity>>;
-  let mut e1 = f.new_object();
+    let factory = Factory::<Entity> {
+        phantom_data: std::marker::PhantomData,
+    };
+    let f = Rc::new(factory) as Rc<dyn FactoryExt<Entity>>;
+    let mut e1 = f.new_object();
 
-  e1.t_i64 = 0x1337833F;
-  let e1_t_i64 = e1.t_i64;
+    e1.t_i64 = 0x1337833F;
+    let e1_t_i64 = e1.t_i64;
 
-  let a1 = Box::new(e1) as Box<dyn AdapterExt>;
+    let a1 = Box::new(e1) as Box<dyn AdapterExt>;
 
-  // flatten
-  a1.to_fb(&mut builder);
-  let data = Vec::from(builder.finished_data());
+    // flatten
+    a1.to_fb(&mut builder);
+    let data = Vec::from(builder.finished_data());
 
-  // inflate
-  let data_slice = data.as_slice();
-  let first_offset: usize = data_slice[0].into();
+    // inflate
+    let data_slice = data.as_slice();
+    let first_offset: usize = data_slice[0].into();
 
-  unsafe {
-    let mut table = flatbuffers::Table::new(data_slice, first_offset);
-    let resurrected_e1 = f.make(&mut table);
+    unsafe {
+        let mut table = flatbuffers::Table::new(data_slice, first_offset);
+        let resurrected_e1 = f.make(&mut table);
 
-    if resurrected_e1.t_i64 == e1_t_i64 {
-      println!("Hello world! {}", resurrected_e1.t_i64);
-    }else {
-      println!("Goodbye cruel world! {}", resurrected_e1.t_i64);
+        if resurrected_e1.t_i64 == e1_t_i64 {
+            println!("Hello world! {}", resurrected_e1.t_i64);
+        } else {
+            println!("Goodbye cruel world! {}", resurrected_e1.t_i64);
+        }
     }
-  }
 }
--- examples/usage_vecs.rs
+++ examples/usage_vecs.rs
@@ -1,63 +1,65 @@
 use std::rc::Rc;
 
 use flatbuffers::FlatBufferBuilder;
-use lean_buffer::{macros::LeanBufferWrite, traits::{Factory, FactoryExt, AdapterExt}};
+use lean_buffer::{
+    macros::LeanBufferWrite,
+    traits::{AdapterExt, Factory, FactoryExt},
+};
 
 // Don't panic when you see this false positive warning:
 // proc macro `LeanBufferWrite` not expanded: proc macro not found in the built dylib
 // Just check if the generated file can be located.
 #[derive(LeanBufferWrite)]
 struct EntityVecs {
-  t_vec_u64: Vec<u64>,
-  t_vec_i64: Vec<i64>,
-  t_vec_u32: Vec<u32>,
-  t_vec_i32: Vec<i32>,
-  t_vec_char: Vec<char>,
-  t_vec_u16: Vec<u16>,
-  t_vec_i16: Vec<i16>,
-  t_vec_u8: Vec<u8>,
-  t_vec_i8: Vec<i8>,
-  t_vec_bool: Vec<bool>,
-  t_vec_string: Vec<String>,
-  t_vec_double: Vec<f64>,
-  t_vec_float: Vec<f32>,
+    t_vec_u64: Vec<u64>,
+    t_vec_i64: Vec<i64>,
+    t_vec_u32: Vec<u32>,
+    t_vec_i32: Vec<i32>,
+    t_vec_char: Vec<char>,
+    t_vec_u16: Vec<u16>,
+    t_vec_i16: Vec<i16>,
+    t_vec_u8: Vec<u8>,
+    t_vec_i8: Vec<i8>,
+    t_vec_bool: Vec<bool>,
+    t_vec_string: Vec<String>,
+    t_vec_double: Vec<f64>,
+    t_vec_float: Vec<f32>,
 }
-
 
 // Either copy this file from your project, or use the name convention
 // `<struct name>_lb_gen.rs` to include the generated file.
 include!(concat!(env!("OUT_DIR"), "/EntityVecs_lb_gen.rs"));
 
 fn main() {
-  let mut builder = FlatBufferBuilder::new();
+    let mut builder = FlatBufferBuilder::new();
 
-  let factory = Factory::<EntityVecs>{
-    phantom_data: std::marker::PhantomData,
-  };
-  let f = Rc::new(factory) as Rc<dyn FactoryExt<EntityVecs>>;
-  let mut e1 = f.new_object();
+    let factory = Factory::<EntityVecs> {
+        phantom_data: std::marker::PhantomData,
+    };
+    let f = Rc::new(factory) as Rc<dyn FactoryExt<EntityVecs>>;
+    let mut e1 = f.new_object();
 
-  let v = vec![0x8,0x3,0x3,0xF];
-  e1.t_vec_i64 = v;
+    let v = vec![0x8, 0x3, 0x3, 0xF];
+    e1.t_vec_i64 = v;
 
-  let a1 = Box::new(e1) as Box<dyn AdapterExt>;
+    let a1 = Box::new(e1) as Box<dyn AdapterExt>;
 
-  // flatten
-  a1.to_fb(&mut builder);
-  let data = Vec::from(builder.finished_data());
+    // flatten
+    a1.to_fb(&mut builder);
+    let data = Vec::from(builder.finished_data());
 
-  // inflate
-  let data_slice = data.as_slice();
-  let first_offset: usize = data_slice[0].into();
+    // inflate
+    let data_slice = data.as_slice();
+    let first_offset: usize = data_slice[0].into();
 
-  unsafe {
-    let mut table = flatbuffers::Table::new(data_slice, first_offset);
-    let resurrected_e1 = f.make(&mut table);
+    unsafe {
+        let mut table = flatbuffers::Table::new(data_slice, first_offset);
+        let resurrected_e1 = f.make(&mut table);
 
-    if resurrected_e1.t_vec_i64 == vec![0x8,0x3,0x3,0xF] {
-      println!("Hello world!");
-    }else {
-      println!("Goodbye cruel world!");
+        if resurrected_e1.t_vec_i64 == vec![0x8, 0x3, 0x3, 0xF] {
+            println!("Hello world!");
+        } else {
+            println!("Goodbye cruel world!");
+        }
     }
-  }
 }
